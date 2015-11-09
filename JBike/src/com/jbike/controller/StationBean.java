5d447113b27e319bca0a5cd139ed5db324be3e0f
Lucio Di Giacomo Noack <lucio.digiacomo@slyt.gba.gov.ar>
2015-11-09 14:01:59 -0300

    footer added

--- JBike/src/com/jbike/controller/StationBean.java
+++ JBike/src/com/jbike/controller/StationBean.java
@@ -14,14 +14,27 @@ import com.jbike.model.Station;
 public class StationBean {
 
 	private List<Station> stations;
+	private Station station;
+
+	private String name;
 
 	@PostConstruct
 	public void init() {
 		stations = new ArrayList<Station>();
-		
+
 		for (int i = 1; i <= 20; i++) {
 			stations.add(new Station(i, "Station " + i));
 		}
+	}
+
+	public String addStation() {
+		Station station = new Station(10, name);
+		stations.add(station);
+		return null;
+	}
+
+	public String editStation(Station station) {
+		return null;
 	}
 
 	public List<Station> getStations() {
@@ -30,5 +43,21 @@ public class StationBean {
 
 	public void setStations(List<Station> stations) {
 		this.stations = stations;
+	}
+
+	public Station getStation() {
+		return station;
+	}
+
+	public void setStation(Station station) {
+		this.station = station;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 }
--- JBike/src/com/jbike/view/StationView.java
+++ JBike/src/com/jbike/view/StationView.java
@@ -4,9 +4,13 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.RowEditEvent;
 
 import com.jbike.controller.StationBean;
 import com.jbike.model.Station;
@@ -28,6 +32,16 @@ public class StationView implements Serializable {
 	@PostConstruct
 	public void init() {
 		stations = stationBean.getStations();
+	}
+
+	public void onRowEdit(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage("Station edited " + ((Station) event.getObject()).getId());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	public void onRowCancel(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage("Edit cancelled " + ((Station) event.getObject()).getId());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
 	}
 
 	public List<Station> getStations() {
