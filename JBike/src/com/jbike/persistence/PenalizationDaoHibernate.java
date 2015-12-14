b7d823108830492879a3a8ea3b0cd2dfe2c55081
Andrés Cimadamore <andres.cimadamore@slyt.gba.gov.ar>
2015-12-14 14:09:03 -0300

    added more queries c:

--- JBike/src/com/jbike/persistence/PenalizationDaoHibernate.java
+++ JBike/src/com/jbike/persistence/PenalizationDaoHibernate.java
@@ -27,13 +27,53 @@ public class PenalizationDaoHibernate extends BaseDaoHibernate<Penalization> imp
 
 	@Override
 	public List<Penalization> findAllCreatedAtBetween(Date from, Date to) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = this.getEntityManager();
+		
+		String sql = "SELECT p FROM Penalization p WHERE 1=1 ";
+		
+		if(from != null){
+			sql += "AND p.createdAt > :from ";
+		}
+		
+		if(to != null){
+			sql += "AND p.createdAt < :to ";
+		}
+		
+		Query q = em.createQuery(sql);
+		
+		if(from != null){
+			q.setParameter("from", from);
+		}
+		if(to != null){
+			q.setParameter("to", to);
+		}
+		
+		return (List<Penalization>) q.getResultList();
 	}
 
 	@Override
 	public List<Penalization> findAllEndedBetween(Date from, Date to) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = this.getEntityManager();
+		
+		String sql = "SELECT p FROM Penalization p WHERE 1=1 ";
+		
+		if(from != null){
+			sql += "AND p.endDate > :from ";
+		}
+		
+		if(to != null){
+			sql += "AND p.endDate < :to ";
+		}
+		
+		Query q = em.createQuery(sql);
+		
+		if(from != null){
+			q.setParameter("from", from);
+		}
+		if(to != null){
+			q.setParameter("to", to);
+		}
+		
+		return (List<Penalization>) q.getResultList();
 	}
 }
