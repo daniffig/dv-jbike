a8b68ee00baa781992c97579442c5f19ef691d87
Lucio Di Giacomo Noack <lucio.digiacomo@slyt.gba.gov.ar>
2015-12-28 12:55:02 -0300

    added email test

--- JBike/src/com/jbike/view/UserView.java
+++ JBike/src/com/jbike/view/UserView.java
@@ -4,7 +4,10 @@ import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.Security;
+import java.util.Date;
 import java.util.List;
+import java.util.Properties;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
@@ -12,11 +15,18 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.xml.bind.DatatypeConverter;
 
 import com.jbike.model.User;
 import com.jbike.persistence.UserDaoHibernate;
 import com.jbike.session.UserSession;
+import com.sun.mail.smtp.SMTPTransport;
 
 @ManagedBean(name = "userView")
 @ViewScoped
@@ -37,7 +47,7 @@ public class UserView implements Serializable {
 
 	@PostConstruct
 	public void init() {
-		//FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+		// FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
 	}
 
 	public UserSession getUserSession() {
@@ -87,6 +97,67 @@ public class UserView implements Serializable {
 				new FacesMessage(FacesMessage.SEVERITY_INFO, "Success!", "Bike successfully saved."));
 
 		return "home";
+	}
+
+	public void emailAndres() throws AddressException, MessagingException {
+		UserView.Send("webmaster.jbike", "jyaa2015", "andrescimadamore@gmail.com", "andrescimadamore@gmail.com", "hola",
+				"hola");
+	}
+
+	public void emailLucio() throws AddressException, MessagingException {
+		UserView.Send("webmaster.jbike", "jyaa2015", "daniffig@gmail.com", "daniffig@gmail.com", "hola",
+				"hola");
+	}
+
+	public static void Send(final String username, final String password, String recipientEmail, String ccEmail,
+			String title, String message) throws AddressException, MessagingException {
+		Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+		final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+
+		// Get a Properties object
+		Properties props = System.getProperties();
+		props.setProperty("mail.smtps.host", "smtp.gmail.com");
+		props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
+		props.setProperty("mail.smtp.socketFactory.fallback", "false");
+		props.setProperty("mail.smtp.port", "465");
+		props.setProperty("mail.smtp.socketFactory.port", "465");
+		props.setProperty("mail.smtps.auth", "true");
+
+		/*
+		 * If set to false, the QUIT command is sent and the connection is
+		 * immediately closed. If set to true (the default), causes the
+		 * transport to wait for the response to the QUIT command.
+		 * 
+		 * ref :
+		 * http://java.sun.com/products/javamail/javadocs/com/sun/mail/smtp/
+		 * package-summary.html
+		 * http://forum.java.sun.com/thread.jspa?threadID=5205249 smtpsend.java
+		 * - demo program from javamail
+		 */
+		props.put("mail.smtps.quitwait", "false");
+
+		Session session = Session.getInstance(props, null);
+
+		// -- Create a new message --
+		final MimeMessage msg = new MimeMessage(session);
+
+		// -- Set the FROM and TO fields --
+		msg.setFrom(new InternetAddress(username + "@gmail.com"));
+		msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientEmail, false));
+
+		if (ccEmail.length() > 0) {
+			msg.setRecipients(Message.RecipientType.CC, InternetAddress.parse(ccEmail, false));
+		}
+
+		msg.setSubject(title);
+		msg.setText(message, "utf-8");
+		msg.setSentDate(new Date());
+
+		SMTPTransport t = (SMTPTransport) session.getTransport("smtps");
+
+		t.connect("smtp.gmail.com", username, password);
+		t.sendMessage(msg, msg.getAllRecipients());
+		t.close();
 	}
 
 	public String logIn() {
