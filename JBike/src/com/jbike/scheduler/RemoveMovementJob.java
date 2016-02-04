a688f56cd40608a49b76bbbea1d779dc3bdde6b1
Andres Cimadamore <andrescimadamore@gmail.com>
2016-02-04 00:54:26 -0300

    fixed RemoveMovementJob :smile:

--- JBike/src/com/jbike/scheduler/RemoveMovementJob.java
+++ JBike/src/com/jbike/scheduler/RemoveMovementJob.java
@@ -10,9 +10,7 @@ import org.quartz.JobExecutionException;
 import com.jbike.model.BikeState;
 import com.jbike.model.Movement;
 import com.jbike.model.MovementState;
-import com.jbike.persistence.BikeDaoHibernate;
 import com.jbike.persistence.FactoryDao;
-import com.jbike.persistence.MovementDaoHibernate;
 import com.jbike.persistence.interfaces.BikeDao;
 import com.jbike.persistence.interfaces.MovementDao;
 
@@ -26,18 +24,18 @@ public class RemoveMovementJob implements Job {
 		
 		for (Movement movement : movementDAO.findAllByState(MovementState.NEW)) {
 			Calendar dueDate = Calendar.getInstance();
-			
+
 			dueDate.setTime(movement.getCreatedAt());
 			dueDate.add(Calendar.HOUR, 3);
 			
 			Date now = new Date(System.currentTimeMillis());
-			
+
 			if (now.after(dueDate.getTime())) {
 				movement.setState(MovementState.CANCELLED);
 				movement.getBike().setState(BikeState.AVAILABLE);
-				
-				bikeDAO.save(movement.getBike());
-				movementDAO.save(movement);
+
+				bikeDAO.update(movement.getBike());
+				movementDAO.update(movement);
 				
 				System.out.println(String.format("Movement %s cancelled.", movement.toString()));
 			}			
