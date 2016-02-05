f375a3513da06c7a1fc59842f621d711f4012cdf
Andres Cimadamore <andrescimadamore@gmail.com>
2016-02-05 00:39:41 -0300

    Fixed BikeTest

--- JBike/src/com/jbike/test/BikeTest.java
+++ JBike/src/com/jbike/test/BikeTest.java
@@ -2,12 +2,16 @@ package com.jbike.test;
 
 
 
+import static org.junit.Assert.*;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import com.jbike.persistence.FactoryDao;
 import com.jbike.persistence.StationDaoHibernate;
 import com.jbike.model.Bike;
+import com.jbike.model.BikeState;
 import com.jbike.model.Station;
 import com.jbike.persistence.BikeDaoHibernate;
 
@@ -45,64 +49,40 @@ public class BikeTest {
 		bdao.deleteAll();
 		sdao.deleteAll();
 	}
-/*
+	
 	@Test
-	public void testFindAllByBrand() {
-		assertTrue(bdao.findAllByBrand("Fire Bird").size() == 1);
+	public void testFindOneByCode(){
+		assertEquals(bdao.findOneByCode("fire_bird_1"), bike_1);
 		
-		assertTrue(bdao.findAllByBrand("Kawasaki").size() == 2);
-		
-		assertTrue(bdao.findAllByBrand("InexistentBrand").isEmpty());
+		assertNull(bdao.findOneByCode("an_inexistent_code"));
 	}
 	
 	@Test
-	public void testFindAllByModel() {
-		List<Bike> bl = bdao.findAllByModel("Raptor");
+	public void testFindAllByName(){
+		assertTrue(bdao.findAllByName("Venzo Raptor").size() == 1);
 		
-		assertTrue(bl.size() == 1);
-		assertEquals(bl.get(0), bike_4);
-		assertEquals(bl.get(0).getModel(), bike_4.getModel());
-		assertEquals(bl.get(0).getBrand(), bike_4.getBrand());
+		assertTrue(bdao.findAllByName("inexistent_name").isEmpty());
 		
-		assertTrue(bdao.findAllByModel("").isEmpty());
+		bike_2.setName("Kawasaki 1");
+		bike_3.setName("Kawasaki 1");
+		
+		bdao.update(bike_2);
+		bdao.update(bike_3);
+		
+		assertTrue(bdao.findAllByName("Kawasaki 1").size() == 2);
 	}
-
+	
 	@Test
-	public void testFindAllByState() {
-		assertEquals(bdao.findAllByState(BikeState.AVAILABLE).size(), 4);
+	public void testFindAllByState(){
+		assertTrue(bdao.findAllByState(BikeState.AVAILABLE).size() == 4);
 		
-		bike_1.setState(BikeState.IN_USE);
-		bike_2.setState(BikeState.REPORTED);
+		assertTrue(bdao.findAllByState(BikeState.UNDER_MAINTENANCE).isEmpty());
 		
-		bdao.update(bike_1);
+		bike_2.setState(BikeState.UNDER_MAINTENANCE);
 		bdao.update(bike_2);
 		
-		assertNotSame(4, bdao.findAllByState(BikeState.AVAILABLE).size());
+		assertTrue(bdao.findAllByState(BikeState.AVAILABLE).size() == 3);
 		
-		List<Bike> bl_in_use = bdao.findAllByState(BikeState.IN_USE);
-		
-		assertEquals(bl_in_use.size(), 1);
-		assertEquals(bl_in_use.get(0), bike_1);
-		
-		List<Bike> bl_reported = bdao.findAllByState(BikeState.REPORTED);
-		
-		assertEquals(bl_reported.size(), 1);
-		assertEquals(bl_reported.get(0), bike_2);
-		
-		bdao.delete(bike_3);
-		
-		assertEquals(bdao.findAllByState(BikeState.AVAILABLE).size(), 1);
+		assertTrue(bdao.findAllByState(BikeState.UNDER_MAINTENANCE).size() == 1);
 	}
-	
-	@Test
-	public void testFindAllRented() {
-		assertTrue(bdao.findAllRented().size() == 0);
-		
-		bike_1.setCurrentStation(null);
-		bdao.update(bike_1);
-		
-		assertTrue(bdao.findAllRented().size() == 1);
-		assertTrue(bdao.findAllRented().contains(bike_1));
-	}
-*/
 }
